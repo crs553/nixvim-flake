d85649159dec65ec82891766fe741b11fd515a15
crs553 <42813301+crs553@users.noreply.github.com>
2025-09-08 20:01:20 +0100

    feat: floaterm added

---
+++ config/plugins/floaterm.nix
@@ -0,0 +1,14 @@
+{
+  plugins.floaterm = {
+    enable = true;
+    settings = {
+      height = 0.7;
+      keymap_kill = "<leader>tk";
+      keymap_new = "<leader>tn";
+      keymap_next = "<leader>tn";
+      keymap_prev = "<leader>tb";
+      keymap_toggle = "<M-i>";
+      opener = "edit";
+    };
+  };
+}
