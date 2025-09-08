9570283364de72fe3362805d66310cd38901e3c9
CS <42813301+crs553@users.noreply.github.com>
2025-09-08 11:11:41 +0100

    feat: colorscheme

---
+++ config/colorscheme.nix
@@ -0,0 +1,25 @@
+{
+  colorschemes = {
+    catppuccin = {
+      enable = true;
+      settings = {
+        flavour = "mocha";
+	#integrations = {
+	#  cmp = true;
+	#  gitsigns = true;
+	#  nvimtree = true;
+	#  treesitter = true;
+	#  mini = {
+	#    enabled = true;
+	#    indentscope_color = "";
+	#  };
+	#  notify = false;
+	#};
+	styles = {
+	  booleans = [ "bold" "italic" ];
+	  conditionals = [ "bold" ];
+	};
+	#term_colors = true;
+      };
+  };
+}
